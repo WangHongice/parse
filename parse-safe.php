93435b81c38df935e0726873059d077559e38fcc
videoparse-api <58383326+videoparse-api@users.noreply.github.com>
2021-02-07 22:36:30 +0800

    Update parse-safe.php

--- parse-safe.php
+++ parse-safe.php
@@ -9,33 +9,6 @@
  *
  * 解析短视频接口 - 安全版
  */
-function curlPost( $url = '', $data ) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url );
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); //部分环境下，需要将参数值设为2，即：curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	$response = curl_exec( $ch );
-	curl_close ( $ch );
-
-	return $response;	
-}
-
-function sign($appId, $appSecret, $url, $timestamp) {
-	$param = [
-		'appid'		=> $appId,
-		'url'		=> $url,
-		'timestamp'	=> $timestamp,
-	];
-	ksort($param);
-	return substr(md5(substr(md5(urldecode(http_build_query($param))), 6, 18) . $appSecret), 10, 16);
-}
-
 //开发者后台生成的appid
 $appId = '';
 
@@ -56,9 +29,34 @@ $param = [
 	'appid'		=> $appId,
 	'url'		=> $url,
 	'timestamp'	=> $timestamp,
-	'sign'		=> $sign,
 ];
+
+$sign = sign($param, $appSecret);
+
+$param['sign'] = $sign;
 
 $apiUrl = 'https://api-sv.videoparse.cn/api/safeparse/parse';
 $videoInfo = curlPost($apiUrl, $param);
 print_r($videoInfo);
+
+function curlPost( $url = '', $data ) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url );
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); //部分环境下，需要将参数值设为2，即：curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	$response = curl_exec( $ch );
+	curl_close ( $ch );
+
+	return $response;	
+}
+
+ function sign($param = [], $appSecret = '') {
+	ksort($param);
+	return substr(md5(substr(md5(urldecode(http_build_query($param))), 6, 18) . $appSecret), 10, 16);
+}
